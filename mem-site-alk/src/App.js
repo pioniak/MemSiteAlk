6e4a173e336c56569eaeb8724c94b6999bbbd951
pioniak <piotrek.golebniak@gmail.com>
2021-06-01 10:21:34 +0200

    Initialized react app

---
+++ mem-site-alk/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          There will be my app content soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
