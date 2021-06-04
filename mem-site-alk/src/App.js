110d0732dbdb40b8067c14fb460f31fa391a1f73
pioniak <piotrek.golebniak@gmail.com>
2021-06-04 23:32:51 +0200

    remove SwitchComponent and MenuComponent, place simple code in App.js

--- mem-site-alk/src/App.js
+++ mem-site-alk/src/App.js
@@ -1,17 +1,35 @@
 import "App.css";
-import MenuComponent from "Components/MenuComponent";
-import { SwitchComponent } from "Components/SwitchComponent";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
+import { HotView } from "Views/HotView";
+import { RegularView } from "Views/RegularView";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div className="NavBar">
-          <MenuComponent />
+          <NavLink className="NavItem" to="/hot">
+            Hot
+          </NavLink>
+          <NavLink className="NavItem" to="/regular">
+            Regular
+          </NavLink>
         </div>
         <div className="Content">
-          <SwitchComponent />
+          <Switch>
+            <Route path="/" exact={true}>
+              <p>HomePage</p>
+            </Route>
+            <Route path="/hot">
+              <HotView />
+            </Route>
+            <Route path="/regular">
+              <RegularView />
+            </Route>
+            <Route path="*">
+              <div>Uuups! Page Not Found! :(</div>
+            </Route>
+          </Switch>
         </div>
       </Router>
     </div>
